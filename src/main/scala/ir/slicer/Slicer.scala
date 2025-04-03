fed7fc49b64f05103fa0e0b19e7f11025b598379
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-04-03 13:49:14 +1000

    Cleanup variables + pull out conversion to Variables file

--- src/main/scala/ir/slicer/Slicer.scala
+++ src/main/scala/ir/slicer/Slicer.scala
@@ -3,37 +3,9 @@ package ir.slicer
 import ir.*
 import ir.eval.evaluateExpr
 
-import ir.transforms.*
-import analysis.IntraProcConstantPropagation
-import analysis.InterProcConstantPropagation
-import analysis.{evaluateExpression, FlatElement}
 import boogie.*
 import analysis.RangeKey
 
-private def convert(mem: Memory, expression: Expr, globals: Map[RangeKey, String]): Option[SlicingParameter] =
-
-  def evalStackVar(arg1: Variable, arg2: Expr): Option[SlicingParameter] =
-    evaluateExpr(arg2) match {
-      case Some(literal: (BitVecLiteral | IntLiteral)) => Some(StackVariable(mem, arg1, literal))
-      case _ => None
-    }
-
-  def evalGlobalVar(e: Expr): Option[SlicingParameter] =
-    evaluateExpr(e) match {
-      case Some(addr: BitVecLiteral) =>
-        globals.find((range, _) => range.start <= addr.value && addr.value <= range.end) match {
-          case Some(_, identifier) => Some(GlobalVariable(mem, addr, identifier))
-          case None => None
-        }
-      case _ => None
-    }
-
-  expression match {
-    case BinaryExpr(BVADD, arg1: Variable, arg2) => evalStackVar(arg1, arg2)
-    case BinaryExpr(BVSUB, arg1: Variable, arg2) => evalStackVar(arg1, UnaryExpr(BVNEG, arg2))
-    case v: Variable => Some(v)
-    case e => evalGlobalVar(e)
-  }
 
 class SlicerDomain(globals: Map[RangeKey, String])
     extends PowerSetDomain[SlicingParameter] {
--- src/main/scala/ir/slicer/Variables.scala
+++ src/main/scala/ir/slicer/Variables.scala
@@ -1,11 +1,54 @@
 package ir.slicer
 
 import ir.*
+import ir.eval.evaluateExpr
+import analysis.RangeKey
 
-case class StackVariable(mem: Memory, variable: Variable, offset: BitVecLiteral | IntLiteral) {
+type AddressLiteral = BitVecLiteral | IntLiteral
+
+case class StackVariable(mem: Memory, variable: Variable, offset: AddressLiteral) {
   override def toString(): String = s"StackVariable($mem, $variable, $offset)"
 }
 
-case class GlobalVariable(mem: Memory, address: BitVecLiteral | IntLiteral, identifier: String) {
+case class GlobalVariable(mem: Memory, address: AddressLiteral, identifier: String) {
   override def toString(): String = s"GlobalVariable($mem, $identifier, $address)"
+}
+
+case class TestVariable(id: String) {
+  override def toString(): String = s"TestVariable(${id})"
+}
+
+def convert(mem: Memory, expression: Expr, globals: Map[RangeKey, String]): Option[SlicingParameter] = {
+
+  def evalStackVar(arg1: Variable, arg2: Expr): Option[SlicingParameter] = {
+    evaluateExpr(arg2) match {
+      case Some(literal: AddressLiteral) => Some(StackVariable(mem, arg1, literal))
+      case _ => None
+    }
+  }
+
+  def evalGlobalVar(e: Expr): Option[SlicingParameter] = {
+    evaluateExpr(e) match {
+      case Some(addr: BitVecLiteral) => {
+        globals.find((range, _) => range.start <= addr.value && addr.value <= range.end) match {
+          case Some(_, identifier) => Some(GlobalVariable(mem, addr, identifier))
+          case None => None
+        }
+      }
+      case Some(addr: IntLiteral) => {
+        globals.find((range, _) => range.start <= addr.value && addr.value <= range.end) match {
+          case Some(_, identifier) => Some(GlobalVariable(mem, addr, identifier))
+          case None => None
+        }
+      }
+      case _ => None
+    }
+  }
+
+  expression match {
+    case BinaryExpr(BVADD, arg1: Variable, arg2) => evalStackVar(arg1, arg2)
+    case BinaryExpr(BVSUB, arg1: Variable, arg2) => evalStackVar(arg1, UnaryExpr(BVNEG, arg2))
+    case v: Variable => Some(v)
+    case e => evalGlobalVar(e)
+  }
 }
