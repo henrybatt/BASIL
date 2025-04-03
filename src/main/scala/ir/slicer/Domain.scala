4bcb2e2f69ab72ae631e0ba33700a358344f0f8f
henrybatt <37820016+henrybatt@users.noreply.github.com>
2025-04-03 13:51:18 +1000

    Convert domain to Lattice for IDE

---
+++ src/main/scala/ir/slicer/Domain.scala
@@ -0,0 +1,67 @@
+package ir.slicer
+
+import ir.*
+import boogie.*
+import analysis.RangeKey
+import analysis.solvers.SimpleWorklistFixpointSolver
+import analysis.*
+
+import analysis.solvers.*
+
+abstract class TestAnalysis(
+  program: Program,
+  globals: Map[RangeKey, String] = Map(),
+  slicingCriterion: Map[CFGPosition, StatementSlice] = Map()
+) extends Analysis[Any] {
+  val lattice: MapLattice[CFGPosition, StatementSlice, PowersetLattice[SlicingParameter]] = MapLattice(
+    PowersetLattice()
+  )
+  val domain: Set[CFGPosition] = Set.empty ++ program
+
+  def transfer(n: CFGPosition, s: StatementSlice): StatementSlice =
+    val extended = s ++ slicingCriterion.getOrElse(n, StatementSlice())
+
+    n match {
+      case p: Procedure => extended
+      case b: Block => extended
+      case a: LocalAssign =>
+        if (extended.contains(a.lhs)) then (extended - a.lhs) ++ a.rhs.variables
+        else extended
+
+      case a: MemoryLoad =>
+        convert(a.mem, a.index, globals)
+        if (extended.contains(a.lhs)) then
+          convert(a.mem, a.index, globals) match {
+            case Some(variable) => (extended - a.lhs) + variable
+            case None => (extended - a.lhs) ++ a.index.variables
+          }
+        else extended
+
+      case m: MemoryStore =>
+        convert(m.mem, m.index, globals) match {
+          case Some(variable) =>
+            if (extended.contains(variable)) then (extended - variable) ++ m.value.variables else extended
+          case None =>
+            if ((Set() ++ m.index.variables).subsetOf(extended)) then
+              (extended -- m.index.variables) ++ m.value.variables
+            else extended
+        }
+
+      case a: Assume => extended ++ a.body.variables
+      case a: Assert => extended ++ a.body.variables
+      case i: IndirectCall => extended
+      case c: DirectCall => extended
+      case g: GoTo => extended
+      case r: Return => extended
+      case r: Unreachable => extended
+      case n: NOP => extended
+    }
+}
+
+class SlicerAnalysis(
+  program: Program,
+  globals: Map[RangeKey, String] = Map(),
+  slicingCriterion: Map[CFGPosition, StatementSlice] = Map()
+) extends TestAnalysis(program, globals, slicingCriterion)
+    with SimpleWorklistFixpointSolver[CFGPosition, StatementSlice, PowersetLattice[SlicingParameter]]
+    with IRIntraproceduralBackwardDependencies
--- src/main/scala/ir/slicer/Slicer.scala
+++ src/main/scala/ir/slicer/Slicer.scala
@@ -6,47 +6,9 @@ import ir.eval.evaluateExpr
 import boogie.*
 import analysis.RangeKey
 
+class Slicer(program: Program, globals: Set[SpecGlobal], globalOffsets: Map[BigInt, BigInt]) {
 
-class SlicerDomain(globals: Map[RangeKey, String])
-    extends PowerSetDomain[SlicingParameter] {
-
-  def transfer(s: StatementSlice, a: Command): StatementSlice =
-    a match {
-      case a: LocalAssign =>
-        if (s.contains(a.lhs)) then (s - a.lhs) ++ a.rhs.variables
-        else s
-
-      case a: MemoryLoad =>
-        convert(a.mem, a.index, globals)
-        if (s.contains(a.lhs)) then
-          convert(a.mem, a.index, globals) match {
-            case Some(variable) => (s - a.lhs) + variable
-            case None => (s - a.lhs) ++ a.index.variables
-          }
-        else s
-
-      case m: MemoryStore =>
-        convert(m.mem, m.index, globals) match {
-          case Some(variable) => if (s.contains(variable)) then (s - variable) ++ m.value.variables else s
-          case None =>
-            if ((Set() ++ m.index.variables).subsetOf(s)) then (s -- m.index.variables) ++ m.value.variables
-            else s
-        }
-
-      case a: Assume => s ++ a.body.variables
-      case a: Assert => s ++ a.body.variables
-      case i: IndirectCall => s
-      case c: DirectCall => s
-      case g: GoTo => s
-      case r: Return => s
-      case r: Unreachable => s
-      case n: NOP => s
-    }
-}
-
-class Slicer(program: Program, globals: Set[SpecGlobal], globalOffsets: Map[BigInt, BigInt]):
-
-  def transform_globals(): Map[RangeKey, String] =
+  private def transformGlobals(): Map[RangeKey, String] = {
     val global = globals
       .map(s => RangeKey(s.address, s.address + (s.size / BigInt(8)) * BigInt(s.arraySize.getOrElse(1)) - 1) -> s.name)
       .toMap
@@ -54,15 +16,13 @@ class Slicer(program: Program, globals: Set[SpecGlobal], globalOffsets: Map[BigI
       case (k, v) if global.find((range, _) => range.start == v).nonEmpty =>
         (RangeKey(k, k), (global.find((range, _) => range.start == v).get).last)
     })
+  }
 
-  def run(): Unit =
-    val domain = SlicerDomain(
-      globals = transform_globals(),
+  def run(): Unit = {
+
+    val slicingCriterion: Map[CFGPosition, StatementSlice] = Map(
     )
 
-    val (entrySet, exitSet) = worklistSolver(domain).solveProc(program.mainProcedure, backwards = true)
+    val results = SlicerAnalysis(program, globals = transformGlobals(), slicingCriterion = slicingCriterion).analyze()
 
-    // entrySet.foreach{
-    //   case (k, v) => Logger.info(s"${k.label}\n\tEntry: ${entrySet(k).size}: ${exitSet(k)} ->\n\tExit: ${v.size}: $v")
-    // }
 
